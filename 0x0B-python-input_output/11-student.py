e8abcb4c1f9c68faa99d4fc12f44d98f2369718b
Anisah Omar <71156452+Anisah-Omar@users.noreply.github.com>
2022-07-05 19:31:12 +0300

    Update 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,32 +1,23 @@
 #!/usr/bin/python3
-"""Contains the clas "Student"""
+""" creating student class """
 
 
 class Student:
-    """Representation of a student"""
+    """ Defining a class"""
     def __init__(self, first_name, last_name, age):
-        """Initializes the student"""
+        """ Initializing a class"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """returns a dictionary representation of a Student instance
-        with specified attributes"""
-        if attrs is None:
+        """ retrieves a dictionary representation """
+        if (type(attrs)) == list and all(type(i) == str for i in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        else:
             return self.__dict__
-        new_dict = {}
-        for a in attrs:
-            try:
-                new_dict[a] = self.__dict__[a]
-            except:
-                pass
-        return new_dict
 
     def reload_from_json(self, json):
-        """replaces all attributes of the Student instance"""
-        for key in json:
-            try:
-                setattr(self, key, json[key])
-            except:
-                pass
+        """update all the attributtes of student"""
+        for i in json:
+            self.__dict__.update({i: json[i]})
